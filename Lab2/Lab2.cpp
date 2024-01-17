f20759d507b64cbf98209e4906045c5cd99962d8
valentiv <valentiv@X-KNOY306PC05>
2024-01-17 12:30:33 -0500

    Funcitionality Achieved

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -7,12 +7,31 @@ int main()
 	float A;
 	float B;
 	float X;
+	
+	cout << "Hello, my name is Vaughn and I'm going to solve the equation:" << endl;
+	cout << "Ax + B = 0" << endl;
+	cout << "For x" << endl;
+
+	cout << " " << endl;
 
 	cout << "Please enter a value for A: ";
 	cin >> A;
-
 	cout << "A=" << A << endl;
 
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+	cout << " " << endl;
+
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	cout << "B=" << B << endl;
+
+	cout << " " << endl;
+
+	cout << "Solving " << A << "x+" << B << "=0..." << endl;
+
+	cout << " " << endl;
+
+	X = -(B) / A;
+
+	cout << "The answer is:" << endl;
+	cout << "x = " << X << endl;
 }
